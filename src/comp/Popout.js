e59635a45f3da9ee3aec3b05c93024fa76a381c1
Victorowsky <Victorowskyy@gmail.com>
2021-06-19 20:14:45 +0200

    useClickAway used to close popout

--- src/comp/Popout.js
+++ src/comp/Popout.js
@@ -1,18 +1,15 @@
 import React, { useCallback } from "react";
 import { useRef } from "react";
 import { CSSTransition } from "react-transition-group";
+import { useClickAway } from "react-use"
 import "./Popout.css";
 const Popout = ({ children, state, setState }) => {
 	const popoutRef = useRef(null);
 
-	const handleClose = useCallback(
-		(e) => {
-			if (!popoutRef.current.contains(e.target)) {
-				setState(false);
-			}
-		},
-		[setState]
-	);
+
+	useClickAway(popoutRef, ()=>{
+		setState(false);
+	})
 
 	return (
 		<CSSTransition
@@ -20,12 +17,6 @@ const Popout = ({ children, state, setState }) => {
 			unmountOnExit
 			in={state}
 			timeout={300}
-			onEnter={() => {
-				document.addEventListener("click", handleClose);
-			}}
-			onExit={() => {
-				document.removeEventListener("click", handleClose);
-			}}
 		>
 			<div className="closeBackground">
 				<div className="popoutContainer" ref={popoutRef}>

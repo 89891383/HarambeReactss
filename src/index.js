2e6a706006932a53ea4b5f72698f6cdeb958dca9
Victorowsky <Victorowskyy@gmail.com>
2021-06-24 15:10:50 +0200

    React 18 Alpha

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import App from "./App";
 
-
-Sentry.init({
-	dsn: "https://261557991ccf4fa6b9dd1aa1ff674ab9@o795817.ingest.sentry.io/5824645",
-	integrations: [new Integrations.BrowserTracing()],
-  
-	// Set tracesSampleRate to 1.0 to capture 100%
-	// of transactions for performance monitoring.
-	// We recommend adjusting this value in production
-	tracesSampleRate: 1.0,
-  });
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -22,3 +10,8 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
+
+// const root = ReactDOM.unstable_createRoot(document.getElementById("root"));
+// root.render(<React.StrictMode>
+// 				<App />
+// 			</React.StrictMode>);
